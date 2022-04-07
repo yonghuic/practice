162df8af14828d5a69c07ee274c3c314f68afd3f
yonghuic <75155638+yonghuic@users.noreply.github.com>
2022-04-07 17:29:15 +0800

    Add files via upload

---
+++ week07-2.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdio.h>
+char line[2000][80];
+
+int compare(const void *p1, const void *p2){
+	return strcmp( (char*)p1, (char*)p2 );
+}
+int main(){
+	int N;
+	scanf("%d\n", &N);
+
+	for(int i=0;i<N;i++){
+		gets(line[i]);
+	}
+	qsort(line, N, 50, compare);
+
+	for(int i=0;i<N;i++){
+		printf("%s\n", line[i]);
+	}
+return 0;
+}

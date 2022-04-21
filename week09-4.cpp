424ff80e0b756922d8bdbc02cc3513d0a6764ac6
yonghuic <75155638+yonghuic@users.noreply.github.com>
2022-04-21 16:50:13 +0800

    Add files via upload

---
+++ week09-4.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+char name[100][80];
+int grade[100];
+int main(){
+	int n;
+	scanf("%d", &n);
+	for(int i=0;i<n;i++){
+		scanf("%s %d", name[i], &grade[i]);
+	}
+
+	for(int k=0;k<n-1;k++){
+		for(int i=0;i<n;i++){
+			if(grade[i] <grade[i+1]){
+				int temp = grade[i];
+				grade[i] = grade[i+1];
+				grade[i+1] = temp;
+			}
+		}
+	}
+	for(int i=0;i<n;i++){
+		printf("%s %d\n", name[i], grade[i]);
+	}
+return 0;
+}

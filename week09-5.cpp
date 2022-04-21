47f4c73ef49723b4bd13cb2e5fb70df56447c0cb
yonghuic <75155638+yonghuic@users.noreply.github.com>
2022-04-21 17:05:53 +0800

    Add files via upload

---
+++ week09-5.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+char name[100][80];
+int grade[100];
+int main(){
+	int n;
+	scanf("%d", &n);
+	for(int i=0;i<n;i++){
+		scanf("%s %d", name[i], &grade[i]);
+	}
+
+	for(int k=0;k<n-1;k++){
+		for(int i=0;i<n;i++){
+			if(grade[i] <grade[i+1]){
+				int temp = grade[i];
+				grade[i] = grade[i+1];
+				grade[i+1] = temp;
+
+				char tempName[80];
+				strcpy(tempName, name[i]);
+				strcpy( name[i], name[i+1]);
+				strcpy( name[i+1], tempName );
+			}
+		}
+	}
+	for(int i=0;i<n;i++){
+		printf("%s %d\n", name[i], grade[i]);
+	}
+return 0;
+}
+
+
+
